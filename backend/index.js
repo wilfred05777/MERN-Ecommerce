d8133a4e5ffe9f4961ec3e05fb3e46bb73151ff0
Wilfred <wilfred5777@gmail.com>
2022-02-10 05:59:59 +0800

    express setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const env = require("./config/envConfig");
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to express" });
 });
 
-const port = 5000;
+const port = env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Your server is running at port number: ${port}`);
+});
+
 // https://www.youtube.com/watch?v=vyghWcWvlAA&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=2
