70431915ca4a4d7f198709e7b72d106a8d28f52b
Wilfred <wilfred5777@gmail.com>
2022-02-11 03:56:13 +0800

    11 react redux config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 // import "./App.css";
 // https://www.youtube.com/watch?v=bvCaVJs6F5c&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=8
 // https://www.youtube.com/watch?v=4O1BpAFD2Uo&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=9
-
+// https://www.youtube.com/watch?v=RroAcbwqZqo&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=10
+// https://www.youtube.com/watch?v=c7kZLGuG53s&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=11
+// https://www.youtube.com/watch?v=MCb1g3MU7Hw&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=12
 import Routing from "./routes/Routing";
 
 function App() {
   return (
-    <div className="">
-      <h2>HEllo World!</h2>
+    <div>
       <Routing />
     </div>
   );
