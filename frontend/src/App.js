4b0e171bf12763e296d8609823186b490aa3591d
Wilfred <wilfred5777@gmail.com>
2022-02-10 11:57:26 +0800

    [09] MERN Stack eCommerce-Setup reactRouterInitial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // import "./App.css";
 // https://www.youtube.com/watch?v=bvCaVJs6F5c&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=8
-
+// https://www.youtube.com/watch?v=4O1BpAFD2Uo&list=PLZdXBpi-l7C7bM_rwTOcWaZv8FInVQYMz&index=9
 function App() {
   return (
     <div className="bg-black">
